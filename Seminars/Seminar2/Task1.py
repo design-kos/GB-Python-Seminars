e18c8542e774fbf063abf4e4eace6bd1a596d85e
«AKoshelya» <design-kos@ya.ru>
2022-09-28 11:10:19 +0500

    Add Task1(Seminar2)

---
+++ Seminars/Seminar2/Task1.py
@@ -0,0 +1,22 @@
+# Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
+
+def InputNumbers(inputText):
+    isNumber = False
+    while not isNumber:
+        try:
+            number = float(input(f"{inputText}"))
+            isNumber = True
+        except ValueError:
+            print("Вы ввели не число")
+    return number
+
+def SumOfDigits(number):
+    sum = 0
+    for i in str(number):
+        if i != ".":
+            sum += int(i)
+    return sum
+
+number = InputNumbers("Введите число: ")
+sum = SumOfDigits(number)
+print(f"Сумма цифр числа {number} равна {sum}")
